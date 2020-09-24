c2aa09e7263aff2622aebf7e3ebd22b856f8aedc
agee1 <orangemadcow@yahoo.com>
2020-09-23 22:20:25 -0700

    Updates

---
+++ precache-manifest.0edbcbedf9888ca36aa016a7277c1ff5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a7c2fb47e759b94ce20b2c2c5f0390ef",
+    "url": "/Country-Info/index.html"
+  },
+  {
+    "revision": "f97d0a011b2ee359ac60",
+    "url": "/Country-Info/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "d2a7912c941051202118",
+    "url": "/Country-Info/static/css/main.239252ee.chunk.css"
+  },
+  {
+    "revision": "f97d0a011b2ee359ac60",
+    "url": "/Country-Info/static/js/2.ff90d7e8.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/Country-Info/static/js/2.ff90d7e8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d2a7912c941051202118",
+    "url": "/Country-Info/static/js/main.b57c8b20.chunk.js"
+  },
+  {
+    "revision": "c3e3231612eaea668610",
+    "url": "/Country-Info/static/js/runtime-main.a2620398.js"
+  }
+]);
