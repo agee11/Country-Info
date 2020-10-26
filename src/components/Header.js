6c45f4e879c9ee57759984e01e79a63954a551b7
agee11 <a.gee5@yahoo.com>
2020-10-26 15:41:46 -0700

    Fix alignment of components so that they are uniform throughout the app

--- src/components/Header.js
+++ src/components/Header.js
@@ -20,7 +20,7 @@ class Header extends React.Component{
     return (
     <Navbar expand="true" bg={this.props.darkmode ? "dark" : "light"} variant={this.props.darkmode ? "dark" : "light"}>
       <Navbar.Brand><strong onClick={() => {this.props.history.push("/Country-Info/")}} className="brand">Where in the world?</strong></Navbar.Brand>
-      <Button onClick={this.handleDarkMode} variant={this.props.darkmode ? "dark" : "light"}><i className="fas fa-moon"></i> Dark Mode</Button>
+      <Button id="dark-toggle" onClick={this.handleDarkMode} variant={this.props.darkmode ? "dark" : "light"}><i className="fas fa-moon"></i> Dark Mode</Button>
     </Navbar>
   )
   }
